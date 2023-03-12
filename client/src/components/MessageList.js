c7758cf7c2300fce1870affd677e89f4a9b4fe34
Ahmed Wasim <ahmedwasim777@gmail.com>
2023-03-12 11:01:02 -0700

    Completed the Functionality

---
+++ client/src/components/MessageList.js
@@ -0,0 +1,25 @@
+import './MessageList.css'
+
+const MessageList = props => {
+  const { messages } = props
+
+  return (
+    <div className="MessageList">
+      {messages.map( message => {
+        const date = new Date(message.createdAt);
+        const formattedDate = date.toLocaleDateString()
+        const formattedTime = date.toLocaleTimeString()
+
+        return(
+        <div className="Message" key={message.id}>
+          <h5>{message.content}</h5>
+          <p className="Message-author">{'user: ' + message.sender.name}</p>
+          <p className="Message-author">{formattedDate + ' ' + formattedTime}</p>
+        </div>
+        )
+})}
+    </div>
+  )
+}
+
+export default MessageList
